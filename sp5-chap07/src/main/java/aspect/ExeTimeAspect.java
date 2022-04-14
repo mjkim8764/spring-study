3cdcf337bff2b926e438412707ee5e11bab8801c
MJKIM <mjkim8764@gmail.com>
2022-04-15 02:54:18 +0900

    Add sp5-chap07 (2) AOP 학습(2)

--- sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
+++ sp5-chap07/src/main/java/aspect/ExeTimeAspect.java
@@ -5,10 +5,12 @@ import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 
 import java.util.Arrays;
 
 @Aspect
+@Order(2)
 public class ExeTimeAspect {
 
     @Pointcut("execution(public * chap07..*(..))")
--- sp5-chap07/src/main/java/main/MainAspect.java
+++ sp5-chap07/src/main/java/main/MainAspect.java
@@ -1,6 +1,6 @@
 package main;
 
-import chap07.RecCalculator;
+import chap07.Calculator;
 import config.AppCtx;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,8 +9,8 @@ public class MainAspect {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppCtx.class);
 
-        // Calculator cal = ctx.getBean("calculator", Calculator.class);
-        RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
+        Calculator cal = ctx.getBean("calculator", Calculator.class);
+        // RecCalculator cal = ctx.getBean("calculator", RecCalculator.class);
 
         long fiveFact = cal.factorial(5);
         System.out.println("cal.factorial(5) = " + fiveFact);
