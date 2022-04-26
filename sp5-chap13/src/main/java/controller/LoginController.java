734cffe541410e8b63f3d77cc365014a467a720a
MJKIM <mjkim8764@gmail.com>
2022-04-26 18:58:43 +0900

    Add sp5-chap13 (3) Cookie 사용 로그인 시에 이메일을 기억하는 Cookie 생성 및 적용

--- sp5-chap13/src/main/java/controller/LoginController.java
+++ sp5-chap13/src/main/java/controller/LoginController.java
@@ -2,6 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,8 @@ import spring.AuthInfo;
 import spring.AuthService;
 import spring.WrongIdPasswordException;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -20,13 +23,27 @@ public class LoginController {
         this.authService = authService;
     }
 
+
+    /**
+     * @CookieValue
+     * value 속성 : 쿠키의 이름을 지정 (여기서는 REMEMBER)
+     * 체크박스를 선택하지 않을 수 있기 때문에 required 는 false 로
+     * (required 가 true 일 때 value 값을 이름으로 하는 쿠키가 없으면 Exception 발생)
+     */
+
     @GetMapping
-    public String form(LoginCommand loginCommand) {
+    public String form(LoginCommand loginCommand,
+                       @CookieValue(value = "REMEMBER", required = false) Cookie rCookie) {
+        if(rCookie != null) {
+            loginCommand.setEmail(rCookie.getValue());
+            loginCommand.setRememberEmail(true);
+        }
         return "login/loginForm";
     }
 
     @PostMapping
-    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session) {
+    public String submit(LoginCommand loginCommand, Errors errors, HttpSession session,
+                         HttpServletResponse response) {
         // new LoginCommandValidator().validate(loginCommand, errors);
         if(errors.hasErrors()) {
             return "login/loginForm";
@@ -38,6 +55,24 @@ public class LoginController {
             );
 
             session.setAttribute("authInfo", authInfo);
+
+
+            /**
+             * 실제 쿠키를 생성하는 코드
+             * submit 메서드의 파라미터로 HttpServletResponse 객체를 생성했고
+             * addCookie 메서드로 쿠키를 넣어줌.
+             */
+
+            Cookie rememberCookie =
+                    new Cookie("REMEMBER", loginCommand.getEmail());
+            rememberCookie.setPath("/");
+            if(loginCommand.isRememberEmail()) {
+                rememberCookie.setMaxAge(60 * 60 * 24 * 30);  // 단위 : 초
+            } else {
+                rememberCookie.setMaxAge(0);
+            }
+
+            response.addCookie(rememberCookie);
 
             return "/login/loginSuccess";
         } catch (WrongIdPasswordException e) {
